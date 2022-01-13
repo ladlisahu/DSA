0cd16f2506e65a472e9ebbc4c139af1485c9b639
ladlisahu <70210864+ladlisahu@users.noreply.github.com>
2022-01-13 16:11:14 +0530

    Add files via upload

---
+++ pattern_15.cpp
@@ -0,0 +1,31 @@
+/*
+Q.3 Pascal’s triangle is a triangular array of the binomial coefficients. 
+Write a function that takes an integer value n as input and prints first n lines of the Pascal’s triangle. 
+Following are the first 6 rows of Pascal’s Triangle.
+1  
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int c=1;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<c<<" ";
+            c=c*(i-j)/j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
